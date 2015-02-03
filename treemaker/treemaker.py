ac391fb298ff69d32ae114d7a048ca06cca8447b
Ben Rosser <bjr@fnal.gov>
2015-02-03 13:36:18 -0600

    Now using a multiprocessing pool.

--- treemaker/treemaker.py
+++ treemaker/treemaker.py
@@ -3,12 +3,17 @@
 import multiprocessing
 import optparse
 import os
+import shutil
 import sys
 
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 from DataFormats.FWLite import Events, Handle
+
+def runOverNtuple(ntuple, output):
+	for event in Events(ntuple):
+		
 
 def runTreemaker(directory, name=""):
 	print "*** Running treemaker over " + directory
@@ -17,6 +22,27 @@ def runTreemaker(directory, name=""):
 	if not ".root" in name:
 		name += ".root"
 	print "*** Output file name = " + name
+
+	outputDir = os.path.join(os.getcwd(), name + "_temp")
+	try:
+		os.mkdir(outputDir)
+	except OSError:
+		print "Error: unable to create temporary output directory."
+		print "Please deal with the directory " + outputDir
+		return
+
+	pool = multiprocessing.Pool()
+
+	for path, dirs, files in os.walk(directory):
+		if path == directory:
+			for ntuple in files:
+				pool.apply_async(runOverNtuple, (os.path.join(path, ntuple), outputDir,))
+
+	pool.join()
+	# For now use os.system:
+	os.system("hadd " + name + " " + outputDir + "/*")
+
+	shutil.rmtree(outputDir)
 
 def main():
 	parser = optparse.OptionParser()
