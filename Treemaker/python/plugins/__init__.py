6fc3f0a3452806688cff940825007f4eb9eeefa6
Ben Rosser <rosser.bjr@gmail.com>
2015-02-28 00:22:12 -0500

    Significantly refactored plugin code. Sorting should happen now, needs testing.

--- Treemaker/python/plugins/__init__.py
+++ Treemaker/python/plugins/__init__.py
@@ -8,40 +8,64 @@ import sys
 
 plugins = []
 
-def loadPlugins(toLoad, useAll=False):
-	# Get a list of all possible plugin names
-	location = os.path.join(getScriptLocation())
+def getPossiblePluginNames(namesToLoad=[], location=os.path.join(getScriptLocation())):
+	"""	Return a list of plugin names that we can try to import."""
 	names = []
 	for path, dirs, files in os.walk(location):
 		if path == location:
 			for filename in files:
-				if not (".pyc" in filename or "__init__" in filename):
-					filename = filename[:-len(".py")]
-					names.append(filename)
-					
-	# Now, use imp to load all the plugins we specified
+				if not ".pyc" in filename or "__init__" in filename:
+					if filename in namesToLoad or namesToLoad == []:
+						filename = filename[:-len(".py")]
+						names.append(filename)
+	return names
+
+def loadPlugins(namesToLoad, useAll=False):
 	global plugins
+	
+	# Get a list of all possible plugin names
+	if useAll:
+		namesToLoad = []
+	names = getPossiblePluginNames(namesToLoad)
+
+	pluginDict = {}
+
+	# Now, use imp to load all the plugins we specified
 	for name in names:
-		if useAll or name in toLoad:
-			print "*** Loading " + name
+		print "*** Loading " + name
+		try:
+			test = sys.modules[name]
+			print "*** Error: a module with the name " + name + " was already loaded!"
+			sys.exit(1)
+		except KeyError:
+			fp, pathname, description = imp.find_module(name, __path__)
 			try:
-				test = sys.modules[name]
-			except KeyError:
-				fp, pathname, description = imp.find_module(name, __path__)
-				try:
-					plugin = imp.load_module(name, fp, pathname, description)
-					plugins.append(plugin)
-				finally:
-					if not fp is None:
-						fp.close()
-					toLoad.remove(name)
+				plugin = imp.load_module(name, fp, pathname, description)
+				pluginDict[name] = plugin
+				plugins.append(plugin)
+			finally:
+				if not fp is None:
+					fp.close()
+
+	# Sort plugins to make sure they got loaded in the right order.
+	i = 0
+	for pluginName in namesToLoad:
+		plugins[i] = pluginDict[pluginName]
+		i += 1
 
 	# Deal with unloaded plugins.
 	# This should be a failure conditional.
-	if len(toLoad) != 0:
+	if len(toLoad) != len(plugins):
 		for name in toLoad:
-			print "ERROR: unable to load plugin " + name
+			if not name in pluginDict.keys():
+				print "ERROR: unable to load plugin " + name
 		sys.exit(1)
+
+	# Then return plugins, just in case.
+	return plugins
+
+def sortPlugins(toLoad):
+	
 
 def createCutsPlugins(cuts):
 	for plugin in plugins:
