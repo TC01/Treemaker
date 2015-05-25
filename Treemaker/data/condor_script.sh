1ec22a8fde55e9a45ce52bd79108636dc2e6b903
Ben Rosser <bjr@fnal.gov>
2015-05-24 22:47:13 -0500

    Hey, checking out the cmssw release works now! Things appear to mostly work.~

--- Treemaker/data/condor_script.sh
+++ Treemaker/data/condor_script.sh
@@ -10,9 +10,12 @@ mkdir -p $OUTPUT_DIR
 # Sadly, it has not been, because it didn't work for some reason
 setenv HOME $_CONDOR_SCRATCH_DIR
 setenv SCRAM_ARCH `scramv1 arch`
+scram project $CMSSW_RELEASE
 cd $CMSSW_RELEASE
 eval `scramv1 runtime -csh`
-cd src && scram build
+cd src
+git clone https://github.com/TC01/Treemaker/
+scram build
 cd $_CONDOR_SCRATCH_DIR
 
 # Run the program here
