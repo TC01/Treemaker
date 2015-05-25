718f56df5406de26db85ca97865332ac6acc95e7
Ben Rosser <bjr@fnal.gov>
2015-05-25 16:00:10 -0500

    It *almost* works, but the config file isn't read for unknown reasons.

--- Treemaker/data/condor_script.sh
+++ Treemaker/data/condor_script.sh
@@ -4,6 +4,7 @@ source job_params
 
 # First, create the output directory.
 set OUTPUT_DIR=$1
+set SPLIT_INDEX=$2
 mkdir -p $OUTPUT_DIR
 
 # This should all be replaced with the script to set up our environment.
@@ -19,7 +20,7 @@ scram build
 cd $_CONDOR_SCRATCH_DIR
 
 # Run the program here
-$CMSSW_RELEASE/src/Treemaker/Treemaker/scripts/treemaker $ARGUMENTS
+$CMSSW_RELEASE/src/Treemaker/Treemaker/scripts/treemaker $ARGUMENTS $SPLIT_INDEX
 
 # Output processing for return
 cd $_CONDOR_SCRATCH_DIR
---
+++ Treemaker/data/hadd_output.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import sys
+
+def main():
+
+	# Occasionally I am a bad programmer too; maybe fix this someday in the future.
+	warning = "Warning! This script is only meant to be ran inside the working directory "
+	warning += "of a condor job created by Treemaker! (https://github.com/TC01/Treemaker)"
+	print warning
+
+	outputFiles = []
+	for path, dir, files in os.walk(os.getcwd()):
+		if "output_" in path:
+			for filename in files:
+				if ".root" in filename:
+					outputFiles.append(os.path.join(path, filename))
+
+	outputPath = os.getcwd()	
+	if len(outputFiles) == 1:
+		shutil.move(outputFiles[0], outputPath)
+	if len(outputFiles) == 0:
+		print "Error: no ROOT files in output directories, please check the logs/"
+		sys.exit(1)
+	else:
+		# Job splitting always begins with IndexX.
+		outputName = outputFiles[0][len("Index0"):]
+		os.system("hadd " + outputName + " output_*/*.root")
+
+	# Clean up and remove the output_* directories.
+	toRemove = []
+	for path, dir, files in os.walk(os.getcwd()):
+		if "output_" in path:
+			toRemove.append(path)
+	for removing in toRemove:
+		shutil.rmtree(removing)
+
+if __name__ == '__main__':
+	main()
