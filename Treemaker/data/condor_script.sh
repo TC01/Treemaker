ff4177bf7bcf6bc73eb9d24834a5dd377700ec25
Ben Rosser <bjr@fnal.gov>
2015-06-07 15:57:24 -0500

    Condor script now correctly copies over plugin directory when running. Close #18.

--- Treemaker/data/condor_script.sh
+++ Treemaker/data/condor_script.sh
@@ -17,6 +17,7 @@ cd $CMSSW_RELEASE
 eval `scramv1 runtime -csh`
 cd src
 git clone https://github.com/TC01/Treemaker/
+cp -r $_CONDOR_SCRATCH_DIR/plugins/* Treemaker/Treemaker/python/plugins/
 scram build
 cd $_CONDOR_SCRATCH_DIR
 
