92627312905abd9120b805675261a95a8d03caeb
Ben Rosser <rosser.bjr@gmail.com>
2016-02-24 00:39:23 -0500

    Fix bugs in zprime_tagjet.

--- Treemaker/python/plugins/zprime_tagjet.py
+++ Treemaker/python/plugins/zprime_tagjet.py
@@ -1,6 +1,7 @@
 # Z' tagjet plugin. The first part of zprime13-treemaker!
 
 import array
+import math
 
 from Treemaker.Treemaker import cuts
 
@@ -23,17 +24,19 @@ def setup(variables, isData):
 
 def analyze(event, variables, leaves, isData, cutArray):
 	numAK8s = leaves['jetAK8_size']
+	tagJet = -1
 	for i in range(min(numAK8s, 4)):		
 		if leaves['jetAK8_prunedMass'][i] > 50 and leaves['jetAK8_Pt'][i] > 200 and math.fabs(leaves['jetAK8_Eta'][0]) < 2.1:
 			tagJet = i
-	variables['tagJetPt'][0] = leaves['jetAK8_Pt'][tagJet]
-	variables['tagJetEta'][0] = leaves['jetAK8_Eta'][tagJet]
-	variables['tagJetPhi'][0] = leaves['jetAK8_Phi'][tagJet]
-	variables['tagJetPrMass'][0] = leaves['jetAK8_prunedMass'][tagJet]
-	variables['tagJetSDMass'][0] = leaves['jetAK8_softDropMass'][tagJet]
-	variables['tagJet_tau1'][0] = leaves['jetAK8_tau1'][tagJet]
-	variables['tagJet_tau2'][0] = leaves['jetAK8_tau2'][tagJet]
-	variables['tagJet_tau3'][0] = leaves['jetAK8_tau3'][tagJet]
+	if tagJet != -1:
+		variables['tagJetPt'][0] = leaves['jetAK8_Pt'][tagJet]
+		variables['tagJetEta'][0] = leaves['jetAK8_Eta'][tagJet]
+		variables['tagJetPhi'][0] = leaves['jetAK8_Phi'][tagJet]
+		variables['tagJetPrMass'][0] = leaves['jetAK8_prunedMass'][tagJet]
+		variables['tagJetSDMass'][0] = leaves['jetAK8_softDropMass'][tagJet]
+		variables['tagJetTau1'][0] = leaves['jetAK8_tau1'][tagJet]
+		variables['tagJetTau2'][0] = leaves['jetAK8_tau2'][tagJet]
+		variables['tagJetTau3'][0] = leaves['jetAK8_tau3'][tagJet]
 	return variables, cutArray
 
 def reset(variables):
@@ -42,9 +45,9 @@ def reset(variables):
 	variables['tagJetPhi'][0] = -99.9
 	variables['tagJetPrMass'][0] = -99.9
 	variables['tagJetSDMass'][0] = -99.9
-	variables['tagJet_tau1'][0] = -99.9
-	variables['tagJet_tau2'][0] = -99.9
-	variables['tagJet_tau3'][0] = -99.9
+	variables['tagJetTau1'][0] = -99.9
+	variables['tagJetTau2'][0] = -99.9
+	variables['tagJetTau3'][0] = -99.9
 	return variables
 
 def createCuts(cutDict):
@@ -53,6 +56,7 @@ def createCuts(cutDict):
 def drop(event, variables, cutArray, leaves, isData):
 	#  If we tag more than one jets, we should drop the event.
 	numAK8s = leaves['jetAK8_size']
+	nTagJets = 0
 	for i in range(min(numAK8s, 4)):		
 		if leaves['jetAK8_prunedMass'][i] > 50 and leaves['jetAK8_Pt'][i] > 200 and math.fabs(leaves['jetAK8_Eta'][0]) < 2.1:
 			nTagJets += 1
