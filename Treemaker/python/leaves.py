08460729511a82b28abb057cc1d7aefd5782bdfd
Ben Rosser <rosser.bjr@gmail.com>
2016-02-23 23:08:54 -0500

    First draft at a leaves.py version of labels.

---
+++ Treemaker/python/leaves.py
@@ -0,0 +1,63 @@
+"""
+Code to retrieve a list of leaves in a given ttree.
+TTree equivalent to labels.py
+Based off of treeviewer code: https://github.com/TC01/rootscripts/blob/master/treeviewer
+"""
+
+import os
+import sys
+
+import ROOT
+
+def getTreeLeaves(tree):
+	leafDict = {}
+	# XXX: I'm not *sure* if this will work.
+	leaves = tree.GetListOfLeaves()
+	for leaf in leaves:
+		leafDict[leaf.GetName()] = None
+	return leafDict
+
+def parseKeyList(tObject, keys, leaves):
+	iterator = ROOT.TListIter(keys)
+	nextKey = iterator.Next()
+	while not nextKey is None and nextKey != None:
+		keyName = nextKey.GetName()
+		try:
+			newObject = tObject.Get(keyName)
+			if isinstance(newObject, ROOT.TTree):
+				subLeaves = getTreeLeaves(newObject)
+				leaves[keyName] = subLeaves
+			elif isinstance(newObject, ROOT.TDirectoryFile):
+				leaves = printKeyList(newObject, newObject.GetListOfKeys(), leaves)
+		except:
+			pass
+		nextKey.Print()
+		nextKey = iterator.Next()
+	return leaves
+
+def getLeaves(rootFile):
+	"""	Given the path to a ROOT file, this returns a dictionary
+		(with all values initialized to zero formatted as follows:
+			leaves[tree][leaf] = None
+	"""
+	leaves = {}
+	try:
+		tFile = ROOT.TFile(os.path.abspath(rootFile))
+		keys = tFile.GetListOfKeys()
+		leaves = parseKeyList(tfile, keys, leaves)
+		tFile.Close()
+	except IndexError:
+		print "Error: invalid ROOT file specified!"
+	except IOError:
+		print "Error: invalid ROOT file specified!"
+	return leaves
+
+# PLEASE NOTE:
+# getLeaves returns leaves[tree][leaf].
+# fillLeaves takes the dictionary tree[leaf], *not* the full leaves dictionary.
+
+def fillLeaves(tree, leaves, index):
+	"""	Given a tree, fill it and fill the leaves (sub)dictionary."""
+	tree.GetEntry(index)
+	for leaf in leaves.iterkeys():
+		leaves[leaf] = eval("tree." + leaf)
